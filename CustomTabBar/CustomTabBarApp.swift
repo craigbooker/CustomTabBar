4057b94b68435295ae961e7ca1cc23264a892cb4
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-03-18 13:39:59 -0500

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Craig Booker on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
